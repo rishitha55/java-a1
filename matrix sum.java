fb04438b87fc76a62235f871074079f45916aade
rishitha55 <47086370+rishitha55@users.noreply.github.com>
2021-09-22 10:32:03 +0530

    Add files via upload

---
+++ matrix sum.java
@@ -0,0 +1,16 @@
+public class MatrixAdditionExample{  
+public static void main(String args[]){  
+   
+int a[][]={{1,2,2},{3,-2,5}};    
+int b[][]={{4,3,0},{7,5,1}};   
+    
+
+int c[][]=new int[3][3];     
+for(int i=0;i<3;i++){    
+for(int j=0;j<3;j++){    
+c[i][j]=a[i][j]+b[i][j];    
+System.out.print(c[i][j]+" ");    
+}    
+System.out.println();
+}    
+}}
