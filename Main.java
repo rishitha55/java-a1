2c3a546dfd841703739bfeb1fcadec91dd5360a8
rishitha55 <47086370+rishitha55@users.noreply.github.com>
2021-09-22 10:31:33 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+
+
+public class Main
+{
+ public static void main(String[] args) {
+  int[][] intArray = { { 1, 2,3 }, { 6, 5,4 }}; 
+  System.out.println("Initialized Two dimensional array:");
+  for (int i = 0; i < 2; i++) { 
+   for (int j = 0; j < 3; j++) { 
+   System.out.print(intArray [i][j] + " "); 
+   } 
+
+   System.out.println(); 
+  } 
+ }
+}
